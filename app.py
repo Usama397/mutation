e59e3d55224cfa958616a3b2be12226d4e25371b
usama397 <usamaashraf127@gmail.com>
2025-04-27 02:58:04 +0500

    Port fixed

--- app.py
+++ app.py
@@ -269,4 +269,4 @@ def managereports():
     return render_template('managereports.html')
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=True)
